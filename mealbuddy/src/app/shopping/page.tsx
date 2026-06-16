b90b955bdb24e0b87016973dfe2ebdc317a38dbc
BlossomBunny <admin@hanafubukidoll.co.uk>
2026-06-16 20:25:09 +0100

    Update page.tsx

--- mealbuddy/src/app/shopping/page.tsx
+++ mealbuddy/src/app/shopping/page.tsx
@@ -46,7 +46,7 @@ export default async function ShoppingPage() {
       .order("created_at", { ascending: false }),
     supabase
       .from("meal_plan")
-      .select("id, planned_for, servings, special, leftover_extra_ingredients, recipe:recipes(id, title, emoji, servings, ingredients)")
+      .select("id, planned_for, servings, special, leftover_extra_ingredients, recipe:recipes!meal_plan_recipe_id_fkey(id, title, emoji, servings, ingredients)")
       .eq("family_id", profile.family_id)
       .gte("planned_for", weekDates[0])
       .lte("planned_for", weekDates[6])
